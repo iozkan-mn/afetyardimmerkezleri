83354876d8907db415a02ad084f61f3fb3615ea7
ismail ozkan <ismail.ozkan@medianova.com>
2023-02-09 23:25:20 +0300

    login, stage model

---
+++ database/migrations/2023_02_09_184653_create_storages_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('storages', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('status')->default(true);
+            $table->timestamp('opening_time')->nullable();
+            $table->timestamp('closing_time')->nullable();
+            $table->text('description');
+            $table->text('address');
+            $table->string('country');
+            $table->string('city');
+            $table->bigInteger('longitude')->nullable();
+            $table->bigInteger('laitude')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('storages');
+    }
+};
